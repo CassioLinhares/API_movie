a8760d7f112ee9c48b786554b50c456344b59387
cassio <cassiolinhares99@gmail.com>
2023-06-09 16:47:17 -0300

    bcryptjs - criptografia

--- src/controllers/users_controllers.js
+++ src/controllers/users_controllers.js
@@ -1,3 +1,19 @@
-const database = require("../database/sqlite");
+const knex = require("../database/knex");
 const AppError = require("../utils/appError");
 
+class userController{
+    
+    async create(request, response){
+        const {name, email, password} = request.body;
+        const checkedUserExists = await knex("users").where({email:email});
+
+        if(checkedUserExists){
+            throw new AppError('E-mail is in use!');
+        }
+        if(!name || !password){
+            throw new AppError('Please, name and password fields are mandatory');
+        }
+
+        await knex("users").insert({name, email, password});
+    }
+}
